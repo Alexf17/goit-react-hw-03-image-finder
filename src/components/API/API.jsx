e3521c5b15c54b03769b7fb42f0768f5691fc219
Oleksii <achili3f17@gmail.com>
2022-10-23 21:38:50 +0300

    fix

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -3,8 +3,9 @@ import { Component } from "react";
 import axios from 'axios';
 
 
-axios.defaults.baseURL = "https://pixabay.com/"
-
+axios.defaults.baseURL = "https://pixabay.com/api"
+const API_KEY = '29780363-e0273b64f82bba5b73a3e8070';
+const FILTERS = 'image_type=photo&orientation=horizontal&per_page=12';
 
 
 export class API extends Component {
@@ -14,16 +15,10 @@ export class API extends Component {
     
         if (prevProps.dataQuery !== this.props.dataQuery || prevProps.dataPage !== this.props.dataPage) {
             
-           const response = await axios.get('api', {
-                                                    params: {
-                                                        q: this.props.dataQuery,
-                                                        key: '29780363-e0273b64f82bba5b73a3e8070',
-                                                        image_type: 'photo',
-                                                        orientation: 'horizontal',
-                                                        per_page: 12,
-                                                        page: this.props.dataPage,
-                                                    },
-                                                    })
+            const response = await axios.get(
+    `?q=${this.props.dataQuery}&page=${this.props.dataPage}&key=${API_KEY}&${FILTERS}`
+  );
+              
     this.props.request(response.data)
 
         }
